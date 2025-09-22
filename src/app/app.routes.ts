a25eab97997d60175db720c9b1158ab0558fb741
Ponos <darukity@Uranus.localdomain>
2025-09-22 23:29:00 +0200

    fix(lazy loading auth) added lazy loading for auth components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { LoginComponent } from './features/auth/components/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   // path vers login et register
-  { path: 'auth/login', component: LoginComponent },
-  // on branchera les features ensuite
-  // { path: 'comics', loadChildren: () => import('./features/comics/comics.routes').then(m => m.comicsRoutes) },
+  { path: 'auth', loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes) },
   { path: '**', redirectTo: '' }
 ];
